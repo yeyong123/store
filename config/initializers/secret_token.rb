27281cbaae1b61ce4306de2802d56cfe79238826
yeyong14 <yeyong14@gmail.com>
2013-05-13 21:41:00 +0800

    完成各组件的安装，先建立框架，对网站布局有初步的定义

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '23b20cde40145685881884cf4924680ad8c080e12fec267cd2ead35404e22ce3b2c2d1dd374ca895cedcf847d1021f81a3311e2a55567a72bd45e8e6454bbf59'
